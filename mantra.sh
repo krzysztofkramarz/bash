91b3203425a5317c037123c1fdbe59d7300d8d62
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-03-12 14:48:51 +0100

    Mantra refactor

--- mantra.sh
+++ mantra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Robie strukturke"
-cd ~/Kod 
+cd ~/Kod/repo 
 
 if [ $# -gt 1 ]
 then
@@ -22,10 +22,10 @@ cd $PROJEKT
 
 ###### MAVEN ######
 touch pom.xml
-mkdir -p src/{main,test}/java/com/epam/$PROJEKT
+mkdir -p src/{main,test}/java/com/fonowizja/$PROJEKT
 curl -o pom.xml https://gist.githubusercontent.com/LIttleAncientForestKami/c9b185c123fc97f6022861f645766aa5/raw/45db276f570fcca357fbcf36b6209517c69c6427/pom.xml
 
-sed -ie "s/<groupId>.*<\/groupId>/<groupId>com.epam.<\/groupId>/g" pom.xml
+sed -ie "s/<groupId>.*<\/groupId>/<groupId>com.fonowizja.<\/groupId>/g" pom.xml
 sed -ie "s/<artifactId>.*<\/artifactId>/<artifactId>$PROJEKT<\/artifactId>/g" pom.xml
 sed -ie "s/<version>.*<\/version>/<version>1.0.0-SNAPSHOT<\/version>/g" pom.xml
 
