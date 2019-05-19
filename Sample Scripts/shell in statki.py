efbd1f677f7d1abddc033b135bf599909c3cba88
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-05-19 19:04:17 +0200

    n

--- Sample Scripts/shell in statki.py
+++ Sample Scripts/shell in statki.py
@@ -1,6 +1,6 @@
 # Enter script code
 import os
 
-os.chdir("/home/krzysztof/Desktop/Kod/repo/statki/Battleships-Server")
+os.chdir("/home/krzysztof/Desktop/Kod/repo/statki/Battleships-Client")
 
 os.system("gnome-terminal")
